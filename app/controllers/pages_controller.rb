80056d7af0bcd8b5086fb2b2e977437577e110bf
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-07-15 02:55:09 -0300

    improved front-end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,21 +8,33 @@ class PagesController < ApplicationController
     @uri = URI(@url)
     @response = Net::HTTP.get(@uri)
     @output = JSON.parse(@response)
-    
+
     if @output.empty?
       @city = "This city does not exist. Try Again."
-    else  
+    else
       @city = @output[0]['ReportingArea']
       @latitude = @output[0]['Latitude']
       @longitude = @output[0]['Longitude']
       @weather_forecast = @output[0]['Category']['Name']
       @aqi = @output[0]['AQI']
 
-      if @aqi < 50
+      if @aqi == "Error"
+        @api_color = "light-grey"
+      elsif @aqi <= 50
         @api_color = "green"
-      end  
+      elsif @aqi >= 51 && @aqi < 100
+        @api_color = "yellow"
+      elsif @aqi >=101 && @aqi < 150
+        @api_color = "orange"
+      elsif @aqi >=151 && @aqi < 200
+        @api_color = "red"
+      elsif @aqi >=201 && @aqi < 300
+        @api_color = "purple"
+      elsif @aqi >=301 && @aqi < 500
+        @api_color = "maroon"
+      end
     end
-    
+
   end
-  
+
 end
