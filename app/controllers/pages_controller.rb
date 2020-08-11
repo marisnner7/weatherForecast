8eb9f26bf007ab426344f4b8f9f364bdf8da4f61
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-08-11 08:54:18 -0300

    refactring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   def home
-
     user_input_city = params[:city]
-
 
     @url = "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=#{user_input_city}&distance=25&API_KEY=0946BB73-C3D4-4AA7-B34A-2EACA50879FF"
     @uri = URI(@url)
@@ -10,7 +8,7 @@ class PagesController < ApplicationController
     @output = JSON.parse(@response)
 
     if @output.empty?
-      @city = "This city does not exist. Try Again."
+      @city = 'This city does not exist. Try Again.'
     else
       @city = @output[0]['ReportingArea']
       @latitude = @output[0]['Latitude']
@@ -18,23 +16,22 @@ class PagesController < ApplicationController
       @weather_forecast = @output[0]['Category']['Name']
       @aqi = @output[0]['AQI']
 
-      if @aqi == "Error"
-        @api_color = "light-grey"
-      elsif @aqi <= 50
-        @api_color = "green"
-      elsif @aqi >= 51 && @aqi < 100
-        @api_color = "yellow"
-      elsif @aqi >=101 && @aqi < 150
-        @api_color = "orange"
-      elsif @aqi >=151 && @aqi < 200
-        @api_color = "red"
-      elsif @aqi >=201 && @aqi < 300
-        @api_color = "purple"
-      elsif @aqi >=301 && @aqi < 500
-        @api_color = "maroon"
-      end
+        case @api_color
+        when 1..50
+          @api_color = 'green'
+        when 51..100
+          @api_color = 'yellow'
+        when 101..150
+          @api_color = 'orange'
+        when 151..200
+          @api_color = 'red'
+        when 201..300
+          @api_color = 'purple'
+        when 301..500
+          @api_color = 'maroon'
+        else
+          @api_color = 'light-grey'
+        end
     end
-
   end
-
 end
